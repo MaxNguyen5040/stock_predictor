cfce8ac25cb723d3e22b8a9745c0f93823392d82
MaxNguyen5040 <maxnguyen3012@gmail.com>
2024-07-02 14:45:46 -0500

    Added interactive plotting with Plotly

--- src/plotter.py
+++ src/plotter.py
@@ -1,10 +1,9 @@
-import matplotlib.pyplot as plt
+import plotly.graph_objects as go
 
 def plot_stock_data(df, ticker):
-    plt.figure(figsize=(10, 5))
-    plt.plot(df['Date'], df['Close'], label='Close Price')
-    plt.title(f'{ticker} Stock Price')
-    plt.xlabel('Date')
-    plt.ylabel('Price')
-    plt.legend()
-    plt.show()
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=df['Date'], y=df['Close'], mode='lines', name='Close Price'))
+    fig.update_layout(title=f'{ticker} Stock Price',
+                      xaxis_title='Date',
+                      yaxis_title='Price')
+    fig.show()
