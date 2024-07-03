b7ab3c7a4c7c94b0662eeb81424e8f1b8e85dd74
MaxNguyen5040 <maxnguyen3012@gmail.com>
2024-07-03 01:19:51 -0500

    Integrated prediction functionality into Dash app

--- src/dash_app.py
+++ src/dash_app.py
@@ -3,6 +3,7 @@ from dash import dcc, html
 from dash.dependencies import Input, Output
 from data_fetcher import fetch_stock_data
 from plotter import plot_stock_data
+from predictor import predict_future_prices
 import plotly.graph_objects as go
 
 app = dash.Dash(__name__)
@@ -14,19 +15,22 @@ app.layout = html.Div([
         start_date='2022-01-01',
         end_date='2023-01-01'
     ),
+    dcc.Input(id='days-ahead', type='number', value=30),
     html.Button('Submit', id='submit-button', n_clicks=0),
     dcc.Graph(id='stock-graph'),
-    dcc.Graph(id='volume-graph')
+    dcc.Graph(id='volume-graph'),
+    dcc.Graph(id='prediction-graph')
 ])
 
 @app.callback(
-    [Output('stock-graph', 'figure'), Output('volume-graph', 'figure')],
+    [Output('stock-graph', 'figure'), Output('volume-graph', 'figure'), Output('prediction-graph', 'figure')],
     [Input('submit-button', 'n_clicks')],
-    [Input('ticker-input', 'value')],
-    [Input('date-picker', 'start_date'), Input('date-picker', 'end_date')]
+    [Input('ticker-input', 'value'), Input('date-picker', 'start_date'), Input('date-picker', 'end_date'), Input('days-ahead', 'value')]
 )
-def update_graph(n_clicks, ticker, start_date, end_date):
+def update_graph(n_clicks, ticker, start_date, end_date, days_ahead):
     df = fetch_stock_data(ticker, start_date, end_date)
+    future_df = predict_future_prices(ticker, start_date, end_date, days_ahead)
+
     fig = go.Figure()
     fig.add_trace(go.Scatter(x=df['Date'], y=df['Close'], mode='lines', name='Close Price'))
     fig.add_trace(go.Scatter(x=df['Date'], y=df['SMA_50'], mode='lines', name='50-Day SMA'))
@@ -41,7 +45,13 @@ def update_graph(n_clicks, ticker, start_date, end_date):
                              xaxis_title='Date',
                              yaxis_title='Volume')
 
-    return fig, fig_volume
+    fig_prediction = go.Figure()
+    fig_prediction.add_trace(go.Scatter(x=future_df['Date'], y=future_df['Predicted_Close'], mode='lines', name='Predicted Close Price'))
+    fig_prediction.update_layout(title=f'{ticker} Predicted Prices for Next {days_ahead} Days',
+                                 xaxis_title='Date',
+                                 yaxis_title='Predicted Close Price')
+
+    return fig, fig_volume, fig_prediction
 
 if __name__ == '__main__':
     app.run_server(debug=True)
